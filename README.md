966afd3c033183cd6dfab98a44a9dbb0c28f4200
ryan <ryantrabalho2@gmail.com>
2024-03-31 17:29:57 -0400

    modificao do slide

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# salgadinho
+## salgadinho
  
+Projeto para estudo para simular a vendas de salgado e doces 
+
+[Curso do Youtube para estudo](https://www.youtube.com/playlist?list=PLYQBQEpR_ZVSycHAW23LCF96ae9EgILMB)
+
--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 include("lib/titulo.php");
 include('includes/head.php');
 include('includes/header.php');
-    
     include('includes/slide.php');
     include('pages/inicial.php');
-
-
 include('includes/footer.php');
 ?>
